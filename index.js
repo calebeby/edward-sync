98dabc0f197a84285c49985b54116a2a88e4a067
Caleb Eby <caleb.eby01@gmail.com>
2017-06-20 15:52:20 +0000

    Add logging

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 
+const ip = '67.169.223.206'
+const port = 3000
+
 const app = express()
 
 app.get('/', (req, res) => res.send('hello'))
 
-app.listen(3000)
+app.listen(port, () => console.log(`http://${ip}:${port}`))
