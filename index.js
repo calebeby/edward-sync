3b3896bbc7930944081330d70c8cf3870b3a6c44
Caleb Eby <caleb.eby01@gmail.com>
2017-06-20 16:55:45 +0000

    Add GitHub webhook handler

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const ip = '67.169.223.206'
 const port = 3000
 
 const app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
 app.get('/', (req, res) => res.send('hello'))
+app.post('/github-webhook', (req, res) => {
+  console.log(req.body)
+  res.send('hi github')
+})
 
 app.listen(port, () => console.log(`http://${ip}:${port}`))
