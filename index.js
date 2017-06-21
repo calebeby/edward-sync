54798cfca0a7d9c4b0e78cb6da359c6e9d22c939
Caleb Eby <caleb.eby01@gmail.com>
2017-06-21 05:02:10 +0000

    Add pulling

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const nodemon = require('nodemon')
+const {spawn} = require('child_process')
+
+const pull = repo => new Promise((resolve, reject) => {
+  const gitPull = spawn('git', ['pull'], {cwd: `../${repo}`})
+  gitPull.stdout.on('data', buf => console.log(buf.toString()))
+  gitPull.stderr.on('data', buf => console.log(buf.toString()))
+  gitPull.on('close', code => {code === 0 ? resolve() : reject(code)})
+})
+
+pull()
+
+const core = nodemon({script: '../edward-core'})
+
+core.on('start', () => console.log('core has started'))
 
 const ip = '67.169.223.206'
 const port = 3000
@@ -13,7 +28,8 @@ app.use(bodyParser.urlencoded({
 
 app.get('/', (req, res) => res.send('hello'))
 app.post('/github-webhook', (req, res) => {
-  console.log(req.body)
+  const repo = req.body.repository.name
+  pull(repo).then(() => core.emit('restart'))
   res.send('hi github')
 })
 
